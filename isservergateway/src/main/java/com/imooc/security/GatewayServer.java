48bb1e3f023d967b8082c842defbd876cec3116b
unknown <jmguoyucheng@126.com>
2019-09-23 06:54:24 +0800

    9-23

---
+++ isservergateway/src/main/java/com/imooc/security/GatewayServer.java
@@ -0,0 +1,13 @@
+package com.imooc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayServer {
+    public static void main(String[] args){
+        SpringApplication.run(GatewayServer.class,args);
+    }
+}
